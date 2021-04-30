e92cecd75d1b9f46547ef0b0d214c1f46567e930
Janet <perezjanet103@gmail.com>
2021-04-30 17:10:47 -0400

    added functions to script.py to read data from sqlite3 db as a df, and to make chart and save it in a file

--- script.py
+++ script.py
@@ -1,7 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from app import db, TreasuryYieldTable
-import datetime
+import datetime as dt
+import pandas as pd
+import sqlite3 as sql
+import numpy as np 
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as path_effects
 
 def scrape():
     url = 'https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=yield'
@@ -52,12 +57,28 @@ def setup_db():
 
 def make_chart(data, filename):
     #I am doing this work in make_chart notebook for now
-    #this code will be to create the chart
+    #this code creates the chart
+    df = data
     print("generating matplotlib chart")
-    plt.plot(data)
+    plt.style.use('bmh')
+    plt.figure(figsize=(15,7))
+    x = ['1 month', '2 month', '3 month', '6 month', '1 year', '2 year', '3 year', '5 year', '7 year', '10 year', '20 year', '30 year']
+    y1 = list(df.iloc[-1][2:])
+    y2 = list(df.iloc[-2][2:])
+    y3 = list(df.iloc[-3][2:])
+    plt.plot(x, y1, 'gs-', label = df.iloc[-1][1],markersize=8, linewidth=3.0, path_effects=[path_effects.SimpleLineShadow(shadow_color="green", linewidth=5),path_effects.Normal()])
+    plt.plot(x, y2, '^-' ,color='magenta', label = df.iloc[-2][1], markersize=6, path_effects=[path_effects.SimpleLineShadow(shadow_color="red", linewidth=5),path_effects.Normal()])
+    plt.plot(x, y3, 'bo-' , label = df.iloc[-3][1], markersize=4, path_effects=[path_effects.SimpleLineShadow(shadow_color="blue", linewidth=5),path_effects.Normal()])
+    plt.title('Treasury Yield Curve')
+    plt.xlabel('Maturity')
+    plt.ylabel('Interest Rates')
+    plt.title('Daily US Treasury Yield Curve')
+    plt.legend(loc='upper left')
+    plt.grid(False)
+    plt.show()
 
     #code to save chart in a file with the date when it was created
-#     plt.savefig(f'charts/{filename}.png')
+    plt.savefig(f'charts/{filename}.png')
     print("completed")
     
 def main():
@@ -66,10 +87,13 @@ def main():
     dt_now = dt.datetime.now()
     dt_fmt = dt_now.strftime("%m-%d-%y-%H%M%S")
     #this is where I'll read the data in the sqlite3 as a df
-    #will pass this data as the first argument in make_chart function  
-    make_chart(np.arange(10), f'test-{dt_fmt}')
-            
+    #will pass this data as the first argument in make_chart function
+    db = 'treasury_yield_curve_rates.db'
+    conn = sql.connect(db) 
+    df = pd.read_sql("SELECT * FROM treasury_yield_table;", conn) 
+    make_chart(df, f'test-{dt_fmt}')
+
 if __name__ == '__main__':
     # scrape()
-    # setup_db()
+    #setup_db()
     main()
